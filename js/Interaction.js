77f989d4f217f225cc62aaaa15539594e952db59
TatumLn <linautatum501@gmail.com>
2023-05-04 20:54:23 +0400

    Portfolio 2eme Commit

--- js/Interaction.js
+++ js/Interaction.js
@@ -1,1 +1,14 @@
-console.log("Hello World")
+console.log("MODE JS Activé")
+
+//Message de salutation
+let salutation = document.getElementById("Salutation")
+salutation.innerText = "</>Hello/Good Night</>"
+
+
+//Presentation
+let whoIAm = document.getElementById("Me")
+whoIAm.innerHTML = "My name is PROSPER <br> I'm a student developer"
+
+//Domaine
+let iDo = document.getElementById("Rôle")
+iDo.innerText = "</>Front & Back_End Mobile & Software </>"
