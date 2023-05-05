804e4191e4d34a6136bfbeca9acf2b386b191178
TatumLn <linautatum501@gmail.com>
2023-05-05 09:45:21 +0400

    Portfolio 3eme Commit

--- js/Interaction.js
+++ js/Interaction.js
@@ -1,15 +1,36 @@
-console.log("MODE JS Activé")
+console.log("Salama Tompoko Oh")
 
+function selecteurID (idName) {
+    return document.getElementById(idName)
+}
+
+function creationElement (elementName) {
+    return document.createElement(elementName)
+}
+
+
+//------------------------Home-------------------------------------------------------------------------------
 //Message de salutation
 let date = new Date;
 let heure = date.getHours();
-let salutation = document.getElementById("Salutation")
+let salutation = selecteurID("Salutation")
 let reponse = (heure < 18)? salutation.innerText = "</>Hello</>":salutation.innerText = "</>Good Night</>"
 
 //Presentation
-let whoIAm = document.getElementById("Me")
-whoIAm.innerHTML = "My name is <span class='S'>PROSPER</span> <br> I'm a <span class='S'>student</span> developer"
+let whoIAm = selecteurID("Me")
+whoIAm.innerHTML = "My name is <span class='S'>PROSPER</span> I'm a <span class='S'>student</span> developer"
 
 //Domaine
-let iDo = document.getElementById("Rôle")
+let iDo = selecteurID("Rôle")
 iDo.innerText = "</>Front & Back_End Mobile & Software </>"
+
+//Telechargement de mon CV
+
+
+//----------------------About--------------------------------------------------------------------------------
+let name = "ZAFINIAINA"
+let surname = "Herma PROSPER"
+let infoPersonnel = selecteurID("personnel")
+let infoEducatif = selecteurID("education")
+let tableUn = creationElement("table")
+let tableDeux = creationElement("table")
